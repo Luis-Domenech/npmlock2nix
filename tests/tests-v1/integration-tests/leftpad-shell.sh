2150e6aabf013af0f391a9449d06aa4b628a5ebd
Luis-Domenech <luisfabiandomenech@gmail.com>
2024-12-05 21:25:09 -0400

    Fixed integration test typos

--- tests/tests-v1/integration-tests/leftpad-shell.sh
+++ tests/tests-v1/integration-tests/leftpad-shell.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-nix-shell $DIR/../tests/examples-projects/single-dependency/shell.nix --run "node -e 'require(\"leftpad\")(123, 7);'"
+nix-shell $DIR/../tests/tests-v1/examples-projects/single-dependency/shell.nix --run "node -e 'require(\"leftpad\")(123, 7);'"
